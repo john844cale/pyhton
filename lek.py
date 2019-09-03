66f8a20cc1b1287d3fc8720fa49592005ea87665
calebhaegg <caleb.hagg@learnet.se>
2019-09-03 11:21:15 +0200

    Lektion tisdag 03/09

---
+++ lek.py
@@ -0,0 +1,10 @@
+# variabler att komma ihåg namn och ålder
+
+# läsa in namn
+name = input("skriv in ditt namn: ")
+# läsa in ålder
+age = input("Hur gammal är du: ")
+# skriva ut hej namn och du är x gammal
+print("Hej", name,", välkommen till vårt program.")
+
+# skriva ut om du är 18 eller inte
