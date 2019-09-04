20363e396a3d2837edfcafc4ed6c48df3d12f870
calebhaegg <caleb.hagg@learnet.se>
2019-09-04 13:24:02 +0200

    onsdag 09/04

---
+++ lek-strings.py
@@ -0,0 +1,34 @@
+firstName = "Caleb"
+middleName = ""
+lastName = "hägg"
+
+# för.efternamn
+# Caleb.Hägg
+# firstname[3].lastname[3]
+
+#print(firstName[0:3].lower() + "" + lastName[0:3].lower() + "19")
+
+userName = firstName[0:3].lower()
+#lastname[0:3].lower()19
+userName = "caleb.hagg@elev.ga.ntig.se"
+print(userName)
+
+
+nickName = "Calle"
+
+
+print(lastName.capitalize())
+print(firstName.upper())
+
+
+
+
+
+tecken = """parenteser(), 
+hakparenteser[],
+ måsvingar{} , 
+ kolon:,
+ semikolon;, 
+ komma ,"""
+#print(tecken[13:137])
+
